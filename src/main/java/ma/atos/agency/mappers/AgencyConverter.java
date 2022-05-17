4ef6159cd0ea5e1861491449454970d8b5e71dff
BOURI-Marouane <marouane.bouri.smi@gmail.com>
2022-05-17 16:43:38 +0200

    FEAT-NoCode[Fix]-merge Agency APi

--- src/main/java/ma/atos/agency/mappers/AgencyConverter.java
+++ src/main/java/ma/atos/agency/mappers/AgencyConverter.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AgencyConverter {
-
+//hello world
     @Autowired
     private ModelMapper modelMapper;
     public Agency toAgency(AgencyDto agencyDto)
